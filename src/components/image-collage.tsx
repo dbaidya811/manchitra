afc311f5471f8e26efc9feb9a1f8a8a49cb07b03
Deep Baidya <dbaidya811@gmail.com>
2025-09-11 16:49:48 +0000

    background image gulo alada alda kore asche oi gulo akste colleg hoye as

--- src/components/image-collage.tsx
+++ src/components/image-collage.tsx
@@ -6,35 +6,29 @@ interface ImageCollageProps {
 }
 
 export function ImageCollage({ images }: ImageCollageProps) {
-  const imageStyles = [
-    { top: '5%', left: '10%', width: 200, height: 300, delay: '0s' },
-    { top: '20%', left: '60%', width: 250, height: 350, delay: '5s' },
-    { top: '50%', left: '25%', width: 180, height: 280, delay: '10s' },
-    { top: '65%', left: '70%', width: 220, height: 320, delay: '15s' },
-    { top: '80%', left: '5%', width: 280, height: 380, delay: '20s' },
-  ];
-
   return (
-    <div className="absolute inset-0 w-full h-full overflow-hidden">
-      <div className="relative w-full h-full">
+    <div className="absolute inset-0 w-full h-full overflow-hidden filter blur-[2px] opacity-50">
+      <div className="grid grid-cols-3 grid-rows-2 h-full w-full gap-px">
         {images.map((src, index) => {
-          const style = imageStyles[index % imageStyles.length];
+          // A simple array to make some images span more rows/cols
+          const spans = [
+            'col-span-1 row-span-1',
+            'col-span-1 row-span-1',
+            'col-span-1 row-span-2', // This one will be taller
+            'col-span-2 row-span-1', // This one will be wider
+            'col-span-1 row-span-1',
+          ];
+
           return (
             <div
               key={index}
-              className="absolute animate-move-down filter blur-[2px]"
-              style={{
-                top: style.top,
-                left: style.left,
-                animationDelay: style.delay,
-              }}
+              className={cn('relative', spans[index % spans.length])}
             >
               <Image
                 src={src}
                 alt={`Collage image ${index + 1}`}
-                width={style.width}
-                height={style.height}
-                className="object-cover rounded-lg shadow-2xl opacity-50"
+                fill
+                className="object-cover"
               />
             </div>
           );
