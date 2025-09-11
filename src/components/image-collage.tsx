d0e170f0d4fed40deb487219cea471fc881bd415
Deep Baidya <dbaidya811@gmail.com>
2025-09-11 16:50:36 +0000

    haa thik ache but ata upto down animation hobe

--- src/components/image-collage.tsx
+++ src/components/image-collage.tsx
@@ -8,31 +8,32 @@ interface ImageCollageProps {
 export function ImageCollage({ images }: ImageCollageProps) {
   return (
     <div className="absolute inset-0 w-full h-full overflow-hidden filter blur-[2px] opacity-50">
-      <div className="grid grid-cols-3 grid-rows-2 h-full w-full gap-px">
-        {images.map((src, index) => {
-          // A simple array to make some images span more rows/cols
-          const spans = [
-            'col-span-1 row-span-1',
-            'col-span-1 row-span-1',
-            'col-span-1 row-span-2', // This one will be taller
-            'col-span-2 row-span-1', // This one will be wider
-            'col-span-1 row-span-1',
-          ];
+      <div className="h-full w-full animate-move-down">
+        <div className="grid grid-cols-3 grid-rows-2 h-full w-full gap-px">
+          {images.map((src, index) => {
+            const spans = [
+              'col-span-1 row-span-1',
+              'col-span-1 row-span-1',
+              'col-span-1 row-span-2', 
+              'col-span-2 row-span-1', 
+              'col-span-1 row-span-1',
+            ];
 
-          return (
-            <div
-              key={index}
-              className={cn('relative', spans[index % spans.length])}
-            >
-              <Image
-                src={src}
-                alt={`Collage image ${index + 1}`}
-                fill
-                className="object-cover"
-              />
-            </div>
-          );
-        })}
+            return (
+              <div
+                key={index}
+                className={cn('relative', spans[index % spans.length])}
+              >
+                <Image
+                  src={src}
+                  alt={`Collage image ${index + 1}`}
+                  fill
+                  className="object-cover"
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
