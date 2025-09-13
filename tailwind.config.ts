b4afbf7d3953596582f6a41e54c8da8802350061
Deep Baidya <dbaidya811@gmail.com>
2025-09-13 05:50:08 +0000

    lodding a image ta aktu choto hobe ar imager tar circle kore sprin ghurb

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -101,13 +101,22 @@ export default {
         'move-down-infinite': {
           '0%': { transform: 'translateY(0)' },
           '100%': { transform: 'translateY(-100%)' },
-        }
+        },
+        spin: {
+          from: {
+            transform: 'rotate(0deg)',
+          },
+          to: {
+            transform: 'rotate(360deg)',
+          },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         'fade-in-up': 'fade-in-up 1s ease-out forwards',
         'move-down-infinite': 'move-down-infinite 50s linear infinite',
+        spin: 'spin 1s linear infinite',
       },
     },
   },
