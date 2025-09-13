ad7211901555a857ab081dcfce1f0b3f4e48b1be
Deep Baidya <dbaidya811@gmail.com>
2025-09-13 15:28:32 +0000

    puro map page juro opensteetmap show korbe

--- src/app/dashboard/map/page.tsx
+++ src/app/dashboard/map/page.tsx
@@ -1,16 +1,11 @@
-import { MobileNav } from "@/components/dashboard/mobile-nav";
-
 export default function DashboardPage() {
   return (
-    <div className="flex min-h-screen flex-col">
-      <main className="flex-1">
-        <iframe
-            src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752051241%2C0.004017949104309083%2C51.47957975293232&layer=mapnik"
-            style={{ border: 0, width: '100%', height: '100%' }}
-            allowFullScreen
-        ></iframe>
-      </main>
-      <MobileNav />
+    <div className="h-screen w-screen">
+      <iframe
+        src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752051241%2C0.004017949104309083%2C51.47957975293232&layer=mapnik"
+        style={{ border: 0, width: '100%', height: '100%' }}
+        allowFullScreen
+      ></iframe>
     </div>
   );
 }
